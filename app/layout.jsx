dbfe5b63bc436eff4a6d583357db9266f8313864
SebaCordova <sebastian.cordova@gmail.com>
2026-03-28 19:14:26 -0300

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "PAES M1 Trainer",
+  description: "Entrenamiento PAES",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
